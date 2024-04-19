002d2e555a468588dfc4c60f8b93c205cbc1e401
makarovaarina <167246597+makarovaarina@users.noreply.github.com>
2024-04-19 20:55:59 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,46 @@
+const myData1 = document.querySelector('.p_nav_day1')
+const myData2 = document.querySelector('.p_nav_day2')
+const myData3 = document.querySelector('.p_nav_day3')
+const myData4 = document.querySelector('.p_nav_day4')
+const myData5 = document.querySelector('.p_nav_day5')
+const myData6 = document.querySelector('.p_nav_day6')
+
+myData1.addEventListener('mouseover', function(){
+    myData1.classList.add('p_nav_day_nav_day_chosen')
+})
+myData1.addEventListener('mouseout', function(){
+    myData1.classList.remove('p_nav_day_nav_day_chosen')
+})
+myData2.addEventListener('mouseover', function(){
+    myData2.classList.add('p_nav_day_nav_day_chosen')
+})
+myData2.addEventListener('mouseout', function(){
+    myData2.classList.remove('p_nav_day_nav_day_chosen')
+})
+myData2.addEventListener('mouseover', function(){
+    myData2.classList.add('p_nav_day_nav_day_chosen')
+})
+myData3.addEventListener('mouseout', function(){
+    myData3.classList.remove('p_nav_day_nav_day_chosen')
+})
+myData3.addEventListener('mouseover', function(){
+    myData3.classList.add('p_nav_day_nav_day_chosen')
+})
+myData4.addEventListener('mouseout', function(){
+    myData4.classList.remove('p_nav_day_nav_day_chosen')
+})
+myData4.addEventListener('mouseover', function(){
+    myData4.classList.add('p_nav_day_nav_day_chosen')
+})
+myData5.addEventListener('mouseout', function(){
+    myData5.classList.remove('p_nav_day_nav_day_chosen')
+})
+myData5.addEventListener('mouseover', function(){
+    myData5.classList.add('p_nav_day_nav_day_chosen')
+})
+myData6.addEventListener('mouseout', function(){
+    myData6.classList.remove('p_nav_day_nav_day_chosen')
+})
+myData6.addEventListener('mouseover', function(){
+    myData6.classList.add('p_nav_day_nav_day_chosen')
+})
